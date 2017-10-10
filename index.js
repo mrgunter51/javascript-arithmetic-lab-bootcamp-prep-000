57a5b03f444201b13a0015a92c1ca2c5455fad8e
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-10 03:19:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,15 +25,15 @@ function preserveDecimal(n) {
 
 function outerFunction() {
   var innerVariable = "I'm sort of a secret.";
- 
+
   return function innerScope() {
     var inaccessible = "Nothing can touch me.";
- 
+
     return innerVariable;
   }
 }
 
 var myScope = outerFunction();
- 
+
 // the stringified version of `innerScope()`
 myScope;
